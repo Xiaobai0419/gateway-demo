05a19a56feb8fb91ca3b8bae3b785153366038d2
Xiaobai0419 <minexiaobai@hotmail.com>
2019-01-10 16:39:36 +0800

    gateway添加后置路由过滤器、全局鉴权过滤器

--- src/main/java/com/sunfield/gateway/config/RouteLocatorConfig.java
+++ src/main/java/com/sunfield/gateway/config/RouteLocatorConfig.java
@@ -13,10 +13,10 @@ public class RouteLocatorConfig {
     public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/test/prefix/**")
-                        .filters(f -> f.stripPrefix(2)
+                        .filters(f -> f.stripPrefix(2)//r.path(“/test/prefix/**”)表示自定义了访问前缀，在真正的Gateway进行路由转发的时候，会用过f.stripPrefix(2)把前缀去掉。
                                 .filter(new CustomFilter())
                                 .addResponseHeader("X-Response-test", "test"))
-                        .uri("lb://SC-CONSUMER")
+                        .uri("lb://SC-CONSUMER")//基于服务发现的服务名访问，注意协议类型为lb://而不是http://（无法路由成功），且gateway默认大写路由服务名
                         .order(0)
                         .id("test_consumer_service")
                 )
--- src/main/java/com/sunfield/gateway/filter/CustomFilter.java
+++ src/main/java/com/sunfield/gateway/filter/CustomFilter.java
@@ -10,6 +10,9 @@ import reactor.core.publisher.Mono;
 
 /**
  * 统计某个或者某种路由的的处理时长
+ * Post类型的Filter
+ * 代码将会在chain.filter(exchange).then()里面的代码运行。
+ * 这个Filter被应用到了特定路由/test/prefix/**上面，其他请求没有被应用，见RouteLocatorConfig路由配置类
  * @author xujin
  */
 public class CustomFilter implements GatewayFilter, Ordered {
